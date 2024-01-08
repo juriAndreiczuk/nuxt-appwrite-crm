2efdd08c6e56ca13f6b53eb0f402fbd8cf21878b
juriAndreiczuk <andreiczuk@tutanota.com>
2024-01-08 18:05:14 +0100

    cards drag and drop

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
   import type { ICard, IColumn } from '@/components/kanban/kanban.types'
   import { useKanbanQuery } from '@/components/kanban/useKanbanQuery'
+  import { useMutation } from '@tanstack/vue-query'
   import dayjs from 'dayjs'
+  import { EnumStatus } from '@/types/deals.types'
 
   useHead({
     title: 'Home'
@@ -9,8 +11,34 @@
 
   const dragCard = ref<ICard | null>(null)
   const sourceColumn = ref<IColumn | null>(null)
-
   const {data, isLoading, refetch} = useKanbanQuery()
+
+  type TypeMutationVariables = {
+    docId: string
+    Status?: EnumStatus
+  }
+
+  const { mutate } = useMutation({
+    mutationKey: ['move card'],
+    mutationFn: ({docId, Status}: TypeMutationVariables) =>
+      DB.updateDocument(DB_ID, COLLECTION_DEALS, docId, {
+        Status
+      }),
+    onSuccess: () => {
+      refetch()
+    }
+  })
+
+  const handleDragStart = (card: ICard, column: IColumn) => {
+    dragCard.value = card
+    sourceColumn.value = column
+  }
+
+  const handleDrop = (targetColumn: IColumn) => {
+    if (dragCard.value && sourceColumn.value) {
+      mutate({ docId: dragCard.value.id, Status: targetColumn.id })
+    }
+  }
 </script>
 
 <template>
@@ -22,6 +50,8 @@
         <div 
           v-for="col of data"
           :key="col.id"
+          @dragover.prevent
+          @drop="handleDrop(col)"
         >
           <div class="py-2 px-5 mb-2 text-center">
             <p class="mb-3 font-light text-lg">{{ col.name }}</p>
@@ -32,6 +62,7 @@
             <Card
               v-for="item of col.items"
               :key="item.id"
+              @dragstart="handleDragStart(item, col)"
               class="mb-3" draggable="true"
             >
               <CardHeader role="button">
